593c35ad8bfb42df8eaffa0c313bcf9f72402ee9
Juneid Teklu <te.juneid@gmail.com>
2024-09-30 20:40:54 -0500

    Give placeShip functionality to place ships on gameboard and throw error if invalid

--- src/Gameboard/gameboard.js
+++ src/Gameboard/gameboard.js
@@ -13,5 +13,24 @@ class Gameboard {
         [{ship: null},{ship: null},{ship: null},{ship: null},{ship: null},{ship: null},{ship: null},{ship: null},{ship: null},{ship: null}]
     ];
   }
+  placeShip(ship, start, end){
+    let notOnTheBoard = (element)=>{
+      if(element > 10 || element< 0){
+        return true;
+      }
+    }
+    if(start.some(notOnTheBoard)||end.some(notOnTheBoard)){
+      throw new Error("Ship must be on the board")
+    }
+
+    if(start[0]==end[0]||start[1]==end[1]){
+    this.gameBoard[start[0]][start[1]].ship = ship;
+    this.gameBoard[end[0]][end[1]].ship = ship;
+    }else{
+      throw new Error("You can only place ships vertically or horizontally")
+    }
+  }
 }
+let player = new Gameboard()
+
 module.exports = Gameboard;
--- src/Gameboard/gameboard.test.js
+++ src/Gameboard/gameboard.test.js
@@ -2,11 +2,27 @@ let Gameboard = require("./gameboard.js");
 let Ship = require("../Ship/ships.js");
 test("Gameboard grid is correctly set up", () => {
   let player = new Gameboard();
-  expect(player.gameBoard[0][0].coor).toBe("A1");
+  expect(typeof player.gameBoard[0][0]).toBe("object");
 });
 test("Places ships in gameboards", () => {
   let player = new Gameboard();
   let patrol = new Ship(2);
-  player.placeShip(patrol, [1, 2], [1, 3]);
-  player.board(/* 1,2-1,3 should have patrol ship */);
+  player.placeShip(patrol, [0, 0], [0, 1]);
+  expect(player.gameBoard[0][0].ship).toStrictEqual(patrol);
+  expect(player.gameBoard[0][1].ship).toStrictEqual(patrol);
 });
+test("placeShip() method does not accept diagonal placements", () => {
+  let player = new Gameboard();
+  let patrol = new Ship(2);
+  expect(() => player.placeShip(patrol, [0, 0], [1, 1])).toThrow(
+    Error("You can only place ships vertically or horizontally")
+  );
+});
+test("Given coordinates must be on the board", () => {
+  let player = new Gameboard();
+  let patrol = new Ship(2);
+  expect(() => player.placeShip(patrol, [10, 10], [11, 11])).toThrow(
+    Error("Ship must be on the board")
+  );
+});
+/* test("All cells must vacant in order to place the ship") */
